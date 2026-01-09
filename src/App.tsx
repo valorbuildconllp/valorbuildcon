863ddb65e21b37ca3d7046bb4c3cf26586284990
Shivam M. Salunkhe <shivamsms102000@gmail.com>
2026-01-09 15:50:29 +0530

    Add route for About page in App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import RCCWorkPage from "./pages/RCCWorkPage";
 import RMCPlantPage from "./pages/RMCPlantPage";
+import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -20,6 +21,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/rcc-work" element={<RCCWorkPage />} />
           <Route path="/rmc-plant" element={<RMCPlantPage />} />
+          <Route path="/about" element={<About />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
